ab67b6ac8c0a299af86eb48f32eb0b13700e43d1
MKLEE <73101332+mkllel@users.noreply.github.com>
2024-02-20 15:13:02 +0900

    Update main.js

--- resources/static/js/main.js
+++ resources/static/js/main.js
@@ -1,24 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     const btnTop = document.getElementById('btn_top');
 
-    // 스크롤 이벤트 리스너 추가
     window.addEventListener('scroll', function() {
         if (window.pageYOffset > 500) {
             btnTop.style.opacity = '1';
             btnTop.style.visibility = 'visible';
         } else {
             btnTop.style.opacity = '0';
-            // 투명도가 0이 되고 나면 버튼을 숨깁니다.
             setTimeout(() => {
                 btnTop.style.visibility = 'hidden';
             }, 500);
         }
     });
-
-    // 버튼 클릭 이벤트 리스너
     btnTop.addEventListener('click', function(event) {
-        event.preventDefault(); // 기본 동작을 방지합니다.
-        window.scrollTo({ top: 0, behavior: 'smooth' }); // 부드러운 스크롤로 맨 위로 이동합니다.
+        event.preventDefault();
+        window.scrollTo({ top: 0, behavior: 'smooth' }); 
     });
 });
 
@@ -39,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     window.addEventListener('scroll', revealCard);
-    revealCard(); // 초기 로드 시 실행
+    revealCard();
 });
 
 
